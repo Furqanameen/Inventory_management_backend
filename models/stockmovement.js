43261c815a1b629c5680f4b6069a54774798fcfc
Your Name <you@example.com>
2025-05-04 23:51:12 +0500

    stock movement model added

---
+++ models/stockmovement.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+const { softDeletePlugin } = require('./plugins/softdelete')
+const { jwtPlugin } = require('./plugins/jwt')
+const { publicFieldsPlugin } = require('./plugins/serializer')
+
+const stockMovementSchema = new mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+      index: true,
+    },
+    store: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Store',
+      required: true,
+      index: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 0,
+      index: true,
+    },
+    stockType: {
+      type: String,
+      enum: ['IN', 'OUT'],
+      required: true,
+      index: true,
+    },
+    comment: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+stockMovementSchema.plugin(jwtPlugin)
+stockMovementSchema.plugin(softDeletePlugin)
+stockMovementSchema.plugin(publicFieldsPlugin, [
+  'product',
+  'store',
+  'quantity',
+  'stockType',
+  'comment',
+])
+
+const StockMovement = mongoose.model('StockMovement', stockMovementSchema)
+
+module.exports = StockMovement
